eae95bc998aeb0a5f8a8eab0a01a2e8d365b2703
MESHACK41 <157960360+MESHACK41@users.noreply.github.com>
2025-12-16 11:59:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,17 +11,17 @@ import moment from 'moment-timezone'
 
 //BETA: If you want to avoid typing the number that will be the bot in the console, add it here:
 //Only applies for option 2 (being a bot with 8-digit text code)
-global.botNumber = '' //Example: 255734980103
+global.botNumber = '' //Example: 255711389698
 
 //*─────────────────────────────*
 
 global.owner = [
 // <-- Number @s.whatsapp.net -->
-  ['255734980103', 'LAZACK28', true],
-  ['255613868502', 'LAZACK28', true],
+  ['255711389698', 'TECH-EXPERT', true],
+  ['255711389698', 'TECH-EXPERT', true],
   
 // <-- Number @lid -->
-  ['', 'lAZACK', true],
+  ['', 'TECH-EXPERT', true],
   ['', '', true], 
   ['', '', true]
 ];
@@ -29,7 +29,7 @@ global.owner = [
 //*─────────────────────────────*
 
 global.mods = []
-global.suittag = ['255734980103'] 
+global.suittag = ['255711389698'] 
 global.prems = []
 
 //*─────────────────────────────*
@@ -37,42 +37,42 @@ global.prems = []
 global.library = 'Baileys'
 global.baileys = 'V 6.7.17' 
 global.vs = '2.2.5'
-global.nameqr = 'LAZACK DEVICE'
-global.namebot = 'LAZACK-DEVICE'
+global.nameqr = 'TECH-EXPERT'
+global.namebot = 'TECH-EXPERT'
 global.sessions = 'session'
 global.jadi = 'JadiBots' 
 global.yukiJadibts = true
 
 //*─────────────────────────────*
 
-global.packname = '⪛✰ LAZACK-DEVICE ✰⪜'
-global.botname = 'LAZACK-DEVICE'
-global.wm = '✿◟LAZACK DEVICE◞✿'
-global.author = 'Made With By LzcTeam'
-global.dev = '© Powered By LzcTeam'
-global.textbot = 'lazackorganisation'
-global.tag = 'lazack28'
+global.packname = '⪛✰ TECH-EXPERT ✰⪜'
+global.botname = 'TECH-EXPERT'
+global.wm = '✿◟TECH-EXPERT◞✿'
+global.author = 'Made With By Tech expertTeam'
+global.dev = '© Powered By Tech ExpertTeam'
+global.textbot = 'MR TECH EXPERT'
+global.tag = 'TECH-EXPERT'
 
 //*─────────────────────────────*
 
-global.currency = 'teamlz'
+global.currency = 'TECH-EXPERT'
 global.welcome1 = '❍ Edit With The Command setwelcome'
 global.welcome2 = '❍ Edit With The Command setbye'
-global.banner = 'https://lazackorganisation.my.id/mtaju.jpg'
-global.avatar = 'https://lazackorganisation.my.id/mtaju.jpg'
+global.banner = 'https://imgur.com/a/zXWVpGK'
+global.avatar = 'https://imgur.com/a/zXWVpGK'
 
 //*─────────────────────────────*
 
-global.gp1 = 'https://chat.whatsapp.com/EATTgyi5jx16HgAggPg8yI?mode=r_c'
-global.community1 = 'https://chat.whatsapp.com/EATTgyi5jx16HgAggPg8yI?mode=r_c'
-global.channel = 'https://whatsapp.com/channel/0029ValRlNGCXC3EussN4b2E'
-global.channel2 = 'https://whatsapp.com/channel/0029ValRlNGCXC3EussN4b2E'
-global.md = 'https://github.com/Lazack28/Lazack-Device'
-global.email = 'lazaromtaju12@gmail.com'
+global.gp1 = 'https://chat.whatsapp.com/K0ATMxcydZj0civwVVjvcu'
+global.community1 = 'https://whatsapp.com/channel/0029VbA1jdkDp2QAvGIIrL0m'
+global.channel = 'https://whatsapp.com/channel/0029VbA1jdkDp2QAvGIIrL0m'
+global.channel2 = 'https://whatsapp.com/channel/0029VbA1jdkDp2QAvGIIrL0m'
+global.md = 'https://github.com/MESHACK41/TECH-EXPERT-MD'
+global.email = 'mrtechexpert07@gmail.com'
 
 //*─────────────────────────────*
 
-global.catalog = fs.readFileSync('./Botify/lazack.jpg');
+global.catalog = fs.readFileSync('https://imgur.com/a/zXWVpGK');
 global.style = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalog, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
 ch1: '120363321705798318@newsletter',
